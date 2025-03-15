390d34e8dd0c42f5e146d5924236462ab71f46bf
Anlin Ma <anlinma@umich.edu>
2025-03-15 16:08:44 -0400

    add timer

---
+++ minesweeper/src/components/Timer/Timer.jsx
@@ -0,0 +1,27 @@
+import { formatTime } from "../../utils/formatTime";
+import React, { useContext, useEffect } from 'react';
+import { GameContext } from '../../context/GameContext';
+import './Timer.css';
+const Timer = () => {
+    const { state, dispatch } = useContext(GameContext);
+    const { timer, isTimerRunning, gameStatus } = state;
+    // Start/stop timer based on game status
+    useEffect(() => {
+        let interval;
+        if (isTimerRunning && gameStatus === 'playing') {
+            interval = setInterval(() => {
+                dispatch({ type: 'INCREMENT_TIMER' });
+            }, 1000);
+        }
+        return () => {
+            if (interval) clearInterval(interval);
+        };
+    }, [isTimerRunning, gameStatus, dispatch]);
+    return (
+        <div className="timer">
+            <span role="img" aria-label="Timer">:stopwatch:</span>
+            <span>{formatTime(timer)}</span>
+        </div>
+    );
+};
+export default Timer;
