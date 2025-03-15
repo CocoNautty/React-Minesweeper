ec95b76d36c8a3bb94f14985b4d89aa463624d0d
Anlin Ma <anlinma@umich.edu>
2025-03-15 16:18:32 -0400

    add gmpvd to app.jxs

--- minesweeper/src/App.jsx
+++ minesweeper/src/App.jsx
@@ -2,21 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { GameProvider } from './context/GameContext'
 
 function App() {
 
   return (
     <div id='App'>
-      <div id='heading container'>
-        <h1 id='title'>MINESWEEPER</h1>
-      </div>
-      <div id='control-panel'>
-        {/* <ControlPanel></ControlPanel> */}
-      </div>
-      <div id='board'>
-        {/* <board></board> */}
-      </div>
-      <footer><p>Yixuan Chen, Anlin Ma &copy; 2025</p></footer>
+      <GameProvider>
+        <div id='heading container'>
+          <h1 id='title'>MINESWEEPER</h1>
+        </div>
+        <div id='control-panel'>
+          {/* <ControlPanel></ControlPanel> */}
+        </div>
+        <div id='board'>
+          {/* <board></board> */}
+        </div>
+        <footer><p>Yixuan Chen, Anlin Ma &copy; 2025</p></footer>
+      </GameProvider>
     </div>
   )
 }
