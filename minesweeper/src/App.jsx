40721059bc1bf976f265a9a324404cda9229e516
Anlin Ma <anlinma@umich.edu>
2025-03-15 15:29:47 -0400

    changed App.jxs, App.css

--- minesweeper/src/App.jsx
+++ minesweeper/src/App.jsx
@@ -4,31 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='App'>
+      <div id='heading container'>
+        <h1 id='title'>MINESWEEPER</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id='control-panel'>
+        {/* <ControlPanel></ControlPanel> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div id='board'>
+        {/* <board></board> */}
+      </div>
+      <div id='leader-board'>
+        {/* <LeaderBoard></LeaderBoard> */}
+      </div>
+      <footer><p>Yixuan Chen, Anlin Ma &copy; 2025</p></footer>
+    </div>
   )
 }
 
