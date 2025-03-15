fbb87ebbcda4049031662eddfaa030773536c8c7
CocoNautty <coconautty@gmail.com>
2025-03-15 15:30:34 -0400

    ./minesweeper/src/constants/difficulties.jsx

---
+++ minesweeper/src/constants/difficulties.jsx
@@ -0,0 +1,26 @@
+export const DIFFICULTIES = {
+    EASY: {
+      name: 'Easy',
+      width: 9,
+      height: 9,
+      mines: 10
+    },
+    MEDIUM: {
+      name: 'Medium',
+      width: 16,
+      height: 16,
+      mines: 40
+    },
+    HARD: {
+      name: 'Hard',
+      width: 30,
+      height: 16,
+      mines: 99
+    },
+    CUSTOM: {
+      name: 'Custom',
+      width: 0,
+      height: 0,
+      mines: 0
+    }
+  };
